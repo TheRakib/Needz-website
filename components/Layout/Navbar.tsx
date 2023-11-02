b2f70eb00365282e066d71d6673f683cb33b40cd
Neamul01 <neamulhaque2002@gmail.com>
2023-11-02 23:26:08 +0600

    tailwind merge added

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -14,6 +14,7 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import AppLogo from "../Shared/AppLogo";
+import { twMerge } from "tailwind-merge";
 
 const pages = [
   "Home",
@@ -58,7 +59,7 @@ function Navbar() {
     return (
       <Box
         sx={{ flexGrow: 0 }}
-        className={`${className && className} gap-2 flex`}
+        className={twMerge(`${className && className} gap-2 flex`)}
       >
         <Button className="bg-primary text-white capitalize">
           Become a partner
--- components/Shared/AppLogo.tsx
+++ components/Shared/AppLogo.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 type Props = {
   className: string;
@@ -14,7 +15,9 @@ function AppLogo({ className, onClick }: Props) {
   return (
     <div
       onClick={handleClick}
-      className={`${className && className} w-[144px] h-[80px] relative`}
+      className={twMerge(
+        `${className && className} w-[144px] h-[80px] relative`
+      )}
     >
       <Image src={"/Logo.png"} fill alt="site logo" />
     </div>
