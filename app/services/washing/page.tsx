ac7ee0a7bb16da851d7d0d3e2cb6164f6d48c6f6
Neamul01 <neamulhaque2002@gmail.com>
2023-11-11 03:49:45 +0600

    plumber all item added

--- app/services/washing/page.tsx
+++ app/services/washing/page.tsx
@@ -1,7 +1,10 @@
 import ServicesBanner from "@/components/Services/ServicesBanner";
+import Agreement from "@/components/Services/Washing/Agreement";
 import CarWash from "@/components/Services/Washing/CarWash";
 import DescriptionWashing from "@/components/Services/Washing/DescriptionWashing";
+import FAQWashing from "@/components/Services/Washing/FAQWashing";
 import Others from "@/components/Services/Washing/Others";
+import ContactUs from "@/components/Shared/ContactUs";
 import React from "react";
 
 export default function page() {
@@ -16,6 +19,9 @@ export default function page() {
       <DescriptionWashing />
       <CarWash />
       <Others />
+      <Agreement />
+      <FAQWashing />
+      <ContactUs />
     </div>
   );
 }
