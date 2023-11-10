e6eb0031b7bb78f523b93c40fba4cbb3ff6a1820
Neamul01 <neamulhaque2002@gmail.com>
2023-11-10 18:20:31 +0600

    service title added

--- components/Services/Plumber/PlumberServices.tsx
+++ components/Services/Plumber/PlumberServices.tsx
@@ -1,5 +1,18 @@
+import SecondTitle from "@/components/Shared/SecondTitle";
 import React from "react";
 
 export default function PlumberServices() {
-  return <div className="max-w-layout mx-auto">PlumberServices</div>;
+  return (
+    <div className="max-w-maxLayout bg-primary text-white py-[90px] my-[120px] mx-auto">
+      <div className="max-w-layout mx-auto">
+        <p className="text-[18px] capitalize text-center text-white/70">
+          Plumbing Services We Offer
+        </p>
+        <SecondTitle
+          title="Trusted plumbers, here to help when you need us most. We adapt our services to meet your unique needs, 24/7."
+          className="max-w-[1000px] mx-auto tracking-tighter !leading-[54px] "
+        />
+      </div>
+    </div>
+  );
 }
