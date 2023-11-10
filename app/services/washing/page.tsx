b1330e9126f31376b241cddcc82d0e3fa831593e
Neamul01 <neamulhaque2002@gmail.com>
2023-11-11 00:47:31 +0600

    washing banner added

--- app/services/washing/page.tsx
+++ app/services/washing/page.tsx
@@ -1,5 +1,15 @@
+import ServicesBanner from "@/components/Services/ServicesBanner";
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <ServicesBanner
+        title="Order a washing machine - we drive all over Stockholm"
+        breadcrumb="Washing machine page"
+        img="/services/washing-machine.png"
+        titleClass="!text-center max-w-[763px]"
+      />
+    </div>
+  );
 }
