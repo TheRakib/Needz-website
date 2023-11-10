ca53bde1e568944ac5f4c27afd28a920d98ff95c
Neamul01 <neamulhaque2002@gmail.com>
2023-11-10 19:10:12 +0600

    service card added

--- components/Services/Plumber/PlumberServices.tsx
+++ components/Services/Plumber/PlumberServices.tsx
@@ -1,18 +1,100 @@
 import SecondTitle from "@/components/Shared/SecondTitle";
+import Image from "next/image";
 import React from "react";
 
 export default function PlumberServices() {
   return (
     <div className="max-w-maxLayout bg-primary text-white py-[90px] my-[120px] mx-auto">
       <div className="max-w-layout mx-auto">
-        <p className="text-[18px] capitalize text-center text-white/70">
-          Plumbing Services We Offer
-        </p>
-        <SecondTitle
-          title="Trusted plumbers, here to help when you need us most. We adapt our services to meet your unique needs, 24/7."
-          className="max-w-[1000px] mx-auto tracking-tighter !leading-[54px] "
-        />
+        <div className="">
+          <p className="text-[18px] capitalize text-center text-white/70">
+            Plumbing Services We Offer
+          </p>
+          <SecondTitle
+            title="Trusted plumbers, here to help when you need us most. We adapt our services to meet your unique needs, 24/7."
+            className="max-w-[1000px] mx-auto tracking-tighter !leading-[54px] "
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-[30px] lg:grid-cols-4">
+          {serviceCardItems.map((item) => (
+            <div
+              key={item.id}
+              className="col-span-1 min-h-[368px] flex flex-col justify-between p-[30px] bg-white text-black rounded-xl"
+            >
+              <div className="flex flex-col gap-4">
+                <div className="flex">
+                  <div className="p-2 shadow-lg shadow-primary/70 rounded-full">
+                    <Image
+                      src={"/home/services/plumber/icon1.svg"}
+                      height={44}
+                      width={44}
+                      alt="service-icon"
+                    />
+                  </div>
+                </div>
+                <h3 className="text-[24px] font-semibold">{item.title}</h3>
+              </div>
+              <div className="flex flex-col">
+                <p className="text-black/70 max-w-[217px]">
+                  {item.description}{" "}
+                  <span className="text-primary underline whitespace-nowrap">
+                    Read More
+                  </span>
+                </p>
+              </div>
+            </div>
+          ))}
+          <div className="col-span-1 min-h-[368px] flex flex-col items-center justify-center p-[30px] text-white rounded-xl text-[30px] ">
+            <p className="text-center">Call now</p>
+            <p className="font-semibold text-center">08-235520</p>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
+const serviceCardItems = [
+  {
+    id: 1,
+    title: "Water Heater",
+    description:
+      "Are you not getting any hot water into the property? Then it is most likely your water heater that is creating problems for you. ",
+  },
+  {
+    id: 2,
+    title: "Heat Pumps",
+    description:
+      "We service and install all types of heat pumps. Choosing a heat pump as a heat source in your home is an investment, not only for your wallet",
+  },
+  {
+    id: 3,
+    title: "Radiators and elements",
+    description:
+      "Are all your radiators cold? Or do they work flawlessly in some rooms and not at all in others? Then you need a plumbing technician! ",
+  },
+  {
+    id: 3,
+    title: "Ventilation",
+    description:
+      "Did you know that it is a plumber who fixes the ventilation? The correct term for plumbing technician actually means heating,",
+  },
+  {
+    id: 4,
+    title: "Pipe Service",
+    description:
+      "Maintenance or repairs of the pipes? No job is too small or too big for us. We also offer service and maintenance agreements for housing",
+  },
+  {
+    id: 5,
+    title: "Planning",
+    description:
+      "Do you have plans to install new pipes? Do you need to upgrade your heating system, or run a drain to your new attic? We help you from start to finish!",
+  },
+  {
+    id: 6,
+    title: "Renovations and new builds",
+    description:
+      "We help you plan for the replacement of everything from mixers to new pipes. When renovating all types of rooms with water and",
+  },
+];
