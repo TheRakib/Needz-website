5221f324a0fb2904b1f3ce13dde8e4b28e9c05b1
Neamul01 <neamulhaque2002@gmail.com>
2023-11-13 21:12:57 +0600

    blog images added

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,14 @@
+import ServicesBanner from "@/components/Services/ServicesBanner";
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <ServicesBanner
+        title="Blog/Newsletter"
+        breadcrumb="Blog/Newsletter page"
+        img="/blog/banner.png"
+      />
+    </div>
+  );
 }
