a17d9129a3f748bbc814db1208a52eaae015dd1c
Neamul01 <neamulhaque2002@gmail.com>
2023-11-15 19:06:28 +0600

    blog page added

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+import BlogCard from "@/components/Blog/BlogCard";
 import Search from "@/components/Blog/Search";
 import Topics from "@/components/Blog/Topics";
 import ServicesBanner from "@/components/Services/ServicesBanner";
 import UserCard from "@/components/Services/UserCard";
+import { Pagination, PaginationItem } from "@mui/material";
 import React from "react";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 export default function page() {
   return (
@@ -19,9 +24,80 @@ export default function page() {
             <Topics />
             <UserCard />
           </div>
-          <div className="max-w-[830px]"></div>
+          <div className="flex flex-col gap-[50px]">
+            <div className="w-full flex flex-col gap-[10px]">
+              {blogItems.map((item) => (
+                <BlogCard key={item.id} item={item} />
+              ))}
+            </div>
+            <div className="flex items-center justify-center w-full">
+              <Pagination
+                count={10}
+                variant="outlined"
+                shape="rounded"
+                renderItem={(item) => (
+                  <PaginationItem
+                    slots={{
+                      previous: () => (
+                        <div>
+                          <ArrowBackIcon />
+                          Previous
+                        </div>
+                      ),
+                      next: () => (
+                        <div className="">
+                          Next
+                          <ArrowForwardIcon />
+                        </div>
+                      ),
+                    }}
+                    {...item}
+                  />
+                )}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 }
+
+const blogItems = [
+  {
+    id: 1,
+    title: "Installing a dishwasher?",
+    text: "Joe. Filker | September 29, 2023",
+    img: "/blog/diswasher.jpg",
+  },
+  {
+    id: 2,
+    title: "Change mixer?",
+    text: "Rose Dummy | September 29, 2023",
+    img: "/blog/mixer.jpg",
+  },
+  {
+    id: 3,
+    title: "Water heater",
+    text: "Joe. Filker | August 16, 2023",
+    img: "/blog/heater.jpg",
+  },
+  {
+    id: 4,
+    title: "Leaking water pipe",
+    text: "Joe. Filker | October 27, 2023",
+    img: "/blog/pipe.jpg",
+  },
+  {
+    id: 5,
+    title: "Change the power outlet yourself?",
+    text: "Rose Dummy | October 27, 2023",
+    img: "/blog/power-outlet.jpg",
+  },
+  {
+    id: 6,
+    title: "Earth-fault circuit breaker trips",
+    text: "Rose Dummy | October 27, 2023",
+    img: "/blog/circuit.png",
+  },
+];
---
+++ components/Blog/BlogCard.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import React from "react";
+
+export default function BlogCard({ item }: Props) {
+  return (
+    <div className="w-full border border-black/20 bg-white hover:bg-primary/10 hover:border-primary/10 rounded-lg cursor-pointer flex gap-6 p-5 ">
+      <div className="w-[258px] h-[170px] relative ">
+        <Image alt="blog" src={item.img} fill />
+      </div>
+      <div className="flex flex-col justify-between">
+        <div className="flex flex-col gap-3">
+          <h3 className="font-semibold text-[24px] ">{item.title}</h3>
+          <p className="text-black/70 ">{item.text}</p>
+        </div>
+        <div className="flex gap-4">
+          <Image
+            alt="facebook"
+            src={"/social/facebook.svg"}
+            height={26}
+            width={26}
+          />
+          <Image
+            alt="twitter"
+            src={"/social/Twitter.svg"}
+            height={26}
+            width={26}
+          />
+          <Image
+            alt="instagram"
+            src={"/social/Instagram.svg"}
+            height={26}
+            width={26}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+type Props = {
+  item: {
+    id: number;
+    title: string;
+    text: string;
+    img: string;
+  };
+};
