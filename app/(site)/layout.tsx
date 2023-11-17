870ba3f7023e2ba3ec77109956a8f3bc50911817
Neamul01 <neamulhaque2002@gmail.com>
2023-11-17 21:23:20 +0600

    studio added

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 // import Script from "next/script";
 import Navbar from "@/components/Layout/Navbar";
 import Footer from "@/components/Layout/Footer";
---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: "mjxpl4qi",
+  dataset: "production",
+  apiVersion: "2023-11-17",
+};
+
+export default config;
