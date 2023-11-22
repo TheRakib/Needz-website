abb94535d297a260ccd5bc712f7191ca91a5de58
Neamul01 <neamulhaque2002@gmail.com>
2023-11-23 03:24:03 +0600

    sendgrid added

--- app/(site)/api/mail/route.ts
+++ app/(site)/api/mail/route.ts
@@ -1,4 +1,52 @@
 import { NextRequest } from "next/server";
+import mail from "@sendgrid/mail";
+
+mail.setApiKey(process.env.SENDGRID_API_KEY as string);
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("hello");
+
+  const body = request.body;
+  let passedValue = await new Response(body).text();
+  let valueToJson = JSON.parse(passedValue);
+
+  const message = `
+  Name: ${valueToJson.name}\r\n
+  Email: ${valueToJson.email}\r\n
+  Message: ${valueToJson.message}
+  `;
+
+  const data = {
+    to: "neamulhaque2002@gmail.com",
+    from: "hello@sunrises.se",
+    subject: "Test web message",
+    text: message,
+    html: message.replace("\r\ng", "<br/>"),
+  };
+
+  (async () => {
+    try {
+      await mail.send(data);
+    } catch (error: any) {
+      console.error(error);
+
+      if (error.response) {
+        console.error(error.response.body);
+      }
+    }
+  })();
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    statusText: "successfully api send",
+  });
+}
+
+// ------------get request
 
 export async function GET(
   request: NextRequest,
@@ -11,25 +59,5 @@ export async function GET(
 
   return new Response("Hello, Next.js!", {
     status: 200,
-  });
-}
-
-export async function POST(
-  request: NextRequest,
-  { params }: { params: { slug: string } }
-) {
-  const searchParams = request.nextUrl.searchParams;
-  const query = searchParams.get("hello");
-
-  const body = request.body;
-
-  let passedValue = await new Response(body).text();
-  let valueToJson = JSON.parse(passedValue);
-
-  console.log(valueToJson);
-
-  return new Response(body, {
-    status: 200,
-    statusText: "successfully api send",
   });
 }
