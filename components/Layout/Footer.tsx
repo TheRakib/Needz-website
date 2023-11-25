09069951d3fae4766ddfc46aa13b0dc5b06a1b68
Neamul01 <neamulhaque2002@gmail.com>
2023-11-25 17:24:30 +0600

    footer done

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -9,42 +9,16 @@ export default function Footer() {
   return (
     <footer className="bg-white text-black pt-[120px] pb-[20px] px-2">
       <div className="max-w-layout mx-auto">
-        <div className="flex justify-between flex-col md:flex-row ">
-          <div className="flex flex-col gap-4">
-            <h2 className="text-[30px] uppercase font-bold text-center md:text-left">
-              subscribe to our newsletter
-            </h2>
-            <p className="text-base text-black/80  text-center md:text-left">
-              Get weekly inspiration in your inbox
-            </p>
-          </div>
-          <div className="flex gap-2 items-center">
-            <InputBase
-              id="outlined-basic"
-              placeholder="Enter you email..."
-              className={` rounded-none border border-black/40 h-[40px] pl-3`}
-            />
-            <div className="">
-              <Button
-                className="bg-primary hover:bg-transparent hover:text-primary text-white rounded-lg py-2 px-4 capitalize font-bold text-base "
-                variant="outlined"
-              >
-                Subscribe
-              </Button>
-            </div>
-          </div>
-        </div>
-        <div className="w-full h-[1px] bg-black/60 my-10 "></div>
         <div className="grid grid-cols-1 md:grid-cols-10 gap-y-4 md:gap-0 justify-between">
           <div className="col-span-2">
             <h3 className="text-[20px] uppercase mb-4 text-center md:text-left ">
-              About us
+              Om oss
             </h3>
             <p className="max-w-[284px] ml-auto md:ml-0 mr-auto text-black/80 text-[14px] leading-6  ">
-              Jour 365 AB offers plumbing and electrical on-call in Stockholm 24
-              hours a day, but we also carry out planned work for private
-              individuals and companies. We use experienced and certified
-              plumbers and electricians.
+              Jour 365 AB erbjuder VVS- och elektrikerjour i Stockholm dygnet
+              runt, men vi utför även planerade arbeten för privatpersoner och
+              företag. Vi använder erfarna och certifierade rörmokare och
+              elektriker.
             </p>
           </div>
           <div className="col-span-4 flex justify-center mr-4">
@@ -56,7 +30,7 @@ export default function Footer() {
                 width={282}
                 className="pt-1"
               />
-              <p className="text-black/70 text-center mt-1 ">Insured With</p>
+              <p className="text-black/70 text-center mt-1 ">Försäkrad med</p>
             </div>
           </div>
           <div className="col-span-4 grid grid-cols-2 gap-1 overflow-x-scroll no-scrollbar">
@@ -85,7 +59,7 @@ export default function Footer() {
         </div>
 
         <div className="flex flex-col gap-4 justify-center items-center mt-16">
-          <div className="flex gap-8 items-center">
+          {/* <div className="flex gap-8 items-center">
             <div className="cursor-pointer">
               <BiLogoFacebook className="text-primary text-[30px] " />
             </div>
@@ -95,9 +69,9 @@ export default function Footer() {
             <div className="cursor-pointer">
               <BsInstagram className="text-[#ff0073] text-[25px] " />
             </div>
-          </div>
+          </div> */}
           <p className="text-[14px] text-black/40 text-center md:text-left">
-            Copyright &copy; 2023 Neovotech Ltd. All rights reserved.
+            Copyright &copy; 2023 Jour365.
           </p>
         </div>
       </div>
@@ -119,37 +93,6 @@ type ContactInfo = {
 };
 
 const contactInfo: ContactInfo[] = [
-  // {
-  //   id: 1,
-  //   title: "Company",
-  //   items: [
-  //     {
-  //       id: 1,
-  //       link: "/about-us",
-  //       title: "About Us",
-  //     },
-  //     {
-  //       id: 2,
-  //       link: "/agreement",
-  //       title: "Services Agreement",
-  //     },
-  //     {
-  //       id: 3,
-  //       link: "/blog",
-  //       title: "Blogs",
-  //     },
-  //     {
-  //       id: 4,
-  //       link: "/",
-  //       title: "Become a Partner",
-  //     },
-  //     {
-  //       id: 5,
-  //       link: "/contact-us",
-  //       title: "Kontakta oss",
-  //     },
-  //   ],
-  // },
   {
     id: 2,
     title: "Out Services",
@@ -183,13 +126,13 @@ const contactInfo: ContactInfo[] = [
   },
   {
     id: 3,
-    title: "Contact Info",
+    title: "Kontakt Info",
     items: [
-      "Jour Elite Sweden AB",
+      "Jour365 AB",
       "Nybodagatan 6B",
-      "171 42 Sonia",
+      "171 42 Solna",
       "Ring: 08-30 22 41",
-      "Email: Order@joureliten.se",
+      "Email: Order@jour365.se",
     ],
   },
 ];
