85c8c2da5dbf6aac3f361dd1d1176a3916aadab4
Neamul01 <neamulhaque2002@gmail.com>
2023-12-30 17:55:08 +0600

    plumber page done

--- components/Services/Plumber/FAQPlumber.tsx
+++ components/Services/Plumber/FAQPlumber.tsx
@@ -19,24 +19,18 @@ const faqItems = [
     id: 1,
     title: "Vad kostar en jour rörmokare?",
     description:
-      "I och med att jourarbetet sker utanför ordinarie arbetstid är priset också något högre. Kontakta oss för att få ett kostnadsförslag.",
+      "Vi erbjuder marknadsmässiga och konkurrenskraftiga priser på alla våra tjänster. ",
   },
   {
     id: 2,
     title: "Vad krävs för att VVS-företag ska vara auktoriserat?",
     description:
-      "För att VVS-företaget ska vara auktoriserat måste de följa alla branschregler som satts upp av organisationen Säkert Vatten. Företaget måste ha en giltig ansvarsförsäkring och vara registrerat hos bolagsverket. Minst en heltidsanställd VVS-montör på företaget måste ha branschlegitimation.",
+      "För att ett VVS-företag skall vara certifierat måste de följa alla branschregler som satts upp av organisationen säkert vatten. Alla våra rörmokare innehar detta certifikat och har flera års erfarenhet inom branschen. ",
   },
   {
     id: 3,
     title: "Kan man utföra VVS-arbeten själv?",
     description:
-      "Det finns ingen lag som förhindrar dig att utföra VVS-arbeten själv så länge du inte ska dra om rör eller avloppssystem. Är du inte helt säker på hur du ska göra kan det snabbt bli ett större problem. Därav är det ofta bättre att låta en VVS-montör utföra arbetet så att det blir gjort på ett korrekt sätt.",
+      "Det finns ingen lag som hindrar privatpersoner från att själva utföra vvs-arbeten, så länge det inte gäller avlopss",
   },
-  // {
-  //   id: 4,
-  //   title: "Är det någon skillnad mellan VVS och rörmokare?",
-  //   description:
-  //     "Enligt lag måste det vara en certifierad rörmokare som drar om rör eller ändrar system. Det finns dock en del VVS-arbeten man kan utföra själv. Det är dock viktigt att man besitter den nödvändiga kunskapen som krävs för att utföra jobbet på ett säkert och korrekt sätt. Inte bara för att arbetet ska hålla måttet men också för att de flesta försäkringsbolag inte täcker olyckor som uppstått efter eget arbete.",
-  // },
 ];
--- components/Services/Plumber/LocalPlumber.tsx
+++ components/Services/Plumber/LocalPlumber.tsx
@@ -9,14 +9,14 @@ export default function LocalPlumber() {
       <div className="">
         <div className="flex flex-col gap-5 items-center justify-center mb-[90px]  max-w-[712px] mx-auto">
           <SecondTitle
-            title="Dina lokala rörmokare i Stockholm - för privatpersoner och företag"
+            title="Rörmokare i stockholm - för privatpersoner och företag."
             className="!text-center"
           />
           <p className="text-[18px] text-black/70 text-center">
-            På jour365 erbjuder vi omfattande VVS-tjänster till både husägare
-            och företag i hela Stockholm. Våra erfarna och certifierade
-            rörmokare är redo att hjälpa dig med allt från mindre installationer
-            till kompletta renoveringar.
+            På Jour365 erbjuder vi omfattande VVS-tjänster för både
+            privapersoner och företag i hela stockholm. Våra erfarna och
+            certifierade är redo att hjälpa dig med allt från minsre
+            reparationer till större installationer.
           </p>
         </div>
         <AccordionPic items={serviceAccordions} withBullets />
@@ -24,8 +24,8 @@ export default function LocalPlumber() {
       <div className="mt-[120px] flex flex-col gap-[70px]">
         <div className="text-[20px] max-w-[1005px] mx-auto">
           <p className="text-center">
-            Våra erfarna och certifierade rörmokare kan hantera alla typer av
-            VVS-problem du kan ha, oavsett hur stora eller små de är.
+            Vi har certifierade och erfarna rörmokare för alla typer av jobb,
+            stora som små. Kontakta oss redan idag för kostnadsförslag!
           </p>
           <p className="text-secondary text-center">
             Kontakta oss idag för en kostnadsfri offert!
@@ -45,9 +45,9 @@ const serviceAccordions = [
     title: "Jour Elektriker",
     img: "/services/plumber/Ventilation.png",
     description: [
-      "Akuta elektriska problem kräver snabba och säkra lösningar. Våra certifierade elektriker är tillgängliga dygnet runt för att hantera alla typer av elrelaterade nödsituationer.",
-      "Vi erbjuder snabb service för elavbrott, säkringsproblem, och andra akuta elproblem för att säkerställa din säkerhet och bekvämlighet.",
-      "Vår jourtjänst täcker allt från små hushållsreparationer till stora elektriska nödlägen, alltid med professionell expertis och snabb respons.",
+      "Elproblem kan uppstå vilken tid på dygnet som helst, och kräver ofta snabba och säkra lösningar. Våra certifierade elektriker är tillgängliga dygnet runt för att hantera alla typer av  elrelaterade problem. Vi hjälper dig med allt från strömavbrott, säkringar som gått till att hela huset står strömlöst.",
+      " Vi strävar alltid efter att vara på plats inom 2h från det att vi fått in en bokning, så att du kan slappna av och luta dig tillbaka.",
+      " Våra jourtjänster täcker allt från mindre reparationer till större installationer. Vi garanterar alltid ett kvalitativt och proffsigt arbete. ",
     ],
   },
   {
@@ -55,9 +55,9 @@ const serviceAccordions = [
     title: "VVS Service",
     img: "/services/plumber/Heat.png",
     description: [
-      "Vi erbjuder ett brett utbud av VVS-tjänster, från installation och underhåll till reparationer och uppgraderingar.",
-      "Vårt team av kvalificerade rörmokare hanterar allt från läckande kranar till större rörinstallationer och renoveringar.",
-      "Vi använder modern utrustning och metoder för att säkerställa högsta kvalitet och effektivitet i alla våra VVS-projekt.",
+      "Vi erbjuder ett brett utbud av VVS-tjänster. Vi utför både nya installationer samt underhåll och service av befintliga installationer. ",
+      "Vi kan hjälpa till med allt från läckande kranar till nya rördragningar. ",
+      "Alla våra rörmokare innehar intyg från säkert vatten och har fera års erfarenhet inom branschen. ",
     ],
   },
   {
@@ -65,9 +65,8 @@ const serviceAccordions = [
     title: "Stopp i Avlopp",
     img: "/services/plumber/Sanitation.png",
     description: [
-      "Stopp i avlopp kan orsaka stora besvär i hemmet. Våra experter är specialiserade på att snabbt identifiera och åtgärda alla typer av avloppsblockeringar.",
-      "Vi använder avancerade tekniker för att effektivt rensa blockeringar och förebygga framtida problem.",
-      "Oavsett om det gäller ett litet handfat eller ett stort avloppssystem, garanterar vi en noggrann och hållbar lösning.",
+      "Stopp i avlopp kan orsaka stora besvär i hemmet. Våra tekniker är utbildade för att snabbt och effektivt identifiera och åtgärda olika typer av stopp.",
+      " Oavsett om det är stopp i ett hand fat eller i flera avlopp samtidigt så garanterar vi snabb och smidig service. ",
     ],
   },
 ];
