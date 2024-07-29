eef16d646648766b51a793f6de11d8742252eb4e
Neamul01 <neamulhaque2002@gmail.com>
2024-07-30 01:49:44 +0600

    sitemap added

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://jour365.se",
+  siteUrl: "https://needz.se",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
